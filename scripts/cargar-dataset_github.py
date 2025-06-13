06d8a132114638ed5e8e1add508067ab51b767e5
Albertooo18 <117312330+Albertooo18@users.noreply.github.com>
2025-06-13 10:14:52 -0500

    Update cargar-dataset_github.py

--- scripts/cargar-dataset_github.py
+++ scripts/cargar-dataset_github.py
@@ -1,1 +1,15 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+#  URL raw del dataset (ya corregida)
+url = "https://raw.githubusercontent.com/Albertooo18/parcial2_ia/main/dataset/trafico_red.csv"
+
+try:
+    # Cargar el dataset
+    df = pd.read_csv(url)
+    print("Dataset cargado correctamente.\n")
+    print(df.head())
+    
+except Exception as e:
+    print("Error al cargar el dataset:", e)
